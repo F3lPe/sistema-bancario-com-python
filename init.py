2f787f89a543d48efa9fde2b61cccff7573d7adc
Felipe do Nascimento <felipegonsalves168@gmail.com>
2026-01-19 15:00:55 -0300

    commit

--- init.py
+++ init.py
@@ -68,5 +68,7 @@ while True:
         break
     if datetime.now() > fim_do_dia:
         total_saques_diarios = 0
+        now = datetime.now()
+        fim_do_dia = datetime(now.year, now.month, now.day, 23, 59, 59)
 
     acoes(input("Escolha uma ação: Realizar depósito [1], Visualizar extrato [2], Realizar Saque [3], Sair [4]"))
